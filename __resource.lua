82e8b11902679fdfc7cc629ca50ff37da537090b
Petr Sopf <trsak1@seznam.cz>
2019-04-08 19:14:35 +0200

    Added locales

--- __resource.lua
+++ __resource.lua
@@ -1,9 +1,16 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
+description 'ESX Inventory HUD'
+
+version '1.0'
+
 ui_page 'html/ui.html'
 
 client_scripts {
+  '@es_extended/locale.lua',
   'client/main.lua',
+  'locales/cs.lua',
+  'locales/en.lua',
   'config.lua'	
 }
 
--- config.lua
+++ config.lua
@@ -2,3 +2,4 @@ Config = {}
 Config.IncludeCash = true -- Include cash in inventory?
 Config.IncludeWeapons = true -- Include weapons in inventory?
 Config.OpenControl = 289 -- Key for opening inventory. Edit html/js/config.js to change key for closing it.
+Config.Locale = "cs"
