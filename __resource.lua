9e45d2d24dbcfe6ce2e7633eebc687f5e5b4f45c
Petr Sopf <trsak1@seznam.cz>
2019-04-08 02:59:10 +0200

    Added basic NUI callbacks

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+ui_page 'html/ui.html'
+
+client_script 'client/main.lua'
+
+files {
+    'html/ui.html',
+    'html/css/contextMenu.min.css',
+    'html/css/ui.css',
+    'html/js/contextMenu.min.js',
+    'html/img/items/burger.png'
+}
