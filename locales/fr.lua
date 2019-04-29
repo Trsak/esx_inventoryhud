caa2d259439c901752d266ff2a0085418d6e5025
Zerefdev <25670700+Zerefdev@users.noreply.github.com>
2019-04-29 12:53:06 +0200

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -2,5 +2,5 @@ Locales['fr'] = {
 	['cash'] = 'Éspèces',
 	['black_money'] = 'Argent sale',
 	['player_nearby'] = 'Le joueur sélectionné n\'est plus près de vous.',
-	['players_nearby'] = 'Il n\'y a ~r~personne~s~ près de vous.',
+	['players_nearby'] = 'Il n\'y a personne près de vous.',
 }
