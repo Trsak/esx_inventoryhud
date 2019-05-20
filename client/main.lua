06f840af0734ffb98e48d2be79b2b0f528d1a4ce
Petr Sopf <trsak1@seznam.cz>
2019-05-20 21:24:16 +0200

    Money fix

--- client/main.lua
+++ client/main.lua
@@ -284,20 +284,18 @@ function loadPlayerInventory()
             weapons = data.weapons
 
             if Config.IncludeCash and money ~= nil and money > 0 then
-                for key, value in pairs(accounts) do
-                    moneyData = {
-                        label = _U("cash"),
-                        name = "cash",
-                        type = "item_money",
-                        count = money,
-                        usable = false,
-                        rare = false,
-                        limit = -1,
-                        canRemove = true
-                    }
+                moneyData = {
+                    label = _U("cash"),
+                    name = "cash",
+                    type = "item_money",
+                    count = money,
+                    usable = false,
+                    rare = false,
+                    limit = -1,
+                    canRemove = true
+                }
 
-                    table.insert(items, moneyData)
-                end
+                table.insert(items, moneyData)
             end
 
             if Config.IncludeAccounts and accounts ~= nil then
