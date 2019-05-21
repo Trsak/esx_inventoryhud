67d46fb3880efc482d78ff540242c73c4055ddad
orloko <sirion_pavanelli@hotmail.com>
2019-05-21 05:20:11 -0300

    Create ptbr.lua

---
+++ locales/ptbr.lua
@@ -0,0 +1,11 @@
+Locales["ptbr"] = {
+	["cash"] = "Dinheiro",
+	["black_money"] = "Dinheiro Sujo",
+	["player_nearby"] = "Essa pessoa não está mais ao seu lado!",
+	["players_nearby"] = "Ninguém por perto!",
+	["openinv_help"] = "Abrir o inventário de outra pessoa",
+	["openinv_id"] = "ID do jogador",
+	["no_permissions"] = "Você não tem permissão para fazer isso!",
+	["no_player"] = "Não encontramos ninguem com esse ID!",
+	["player_inventory"] = "Inventário do jogador"
+}
