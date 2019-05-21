a31c7f907d3b6373c7b41469ad784b0b4a7aa0e2
orloko <sirion_pavanelli@hotmail.com>
2019-05-21 05:20:00 -0300

    Create ptbr.lua

---
+++ html/locales/ptbr.lua
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Soltar";
+invLocale.useItem = "Usar";
+invLocale.giveItem = "Dar";
+invLocale.secondInventoryNotAvailable = "Segundo inventário não disponível";
