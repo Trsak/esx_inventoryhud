b5546f66095f9b983f0138f91293ed30f54d9e16
HarryElSuzio <h4rr1p3t4s@gmail.com>
2019-05-24 15:42:54 +0200

    add spanish
    add spanish

---
+++ locales/es.lua
@@ -0,0 +1,11 @@
+Locales["es"] = {
+	["cash"] = "Efectivo",
+	["black_money"] = "Dinero Negro",
+	["player_nearby"] = "¡Este jugador ya no está cerca de ti!",
+	["players_nearby"] = "¡No hay jugadores cerca!",
+	["openinv_help"] = "Abre el inventario de otro jugador.",
+	["openinv_id"] = "ID del jugador",
+	["no_permissions"] = "¡No tienes permisos para hacer eso!",
+	["no_player"] = "¡No encontré jugador con ID dada!",
+	["player_inventory"] = "Inventario de jugadores"
+}
