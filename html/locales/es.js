7e7b743ed169e368263839db4197280d2fed1ba5
HarryElSuzio <h4rr1p3t4s@gmail.com>
2019-05-24 15:44:26 +0200

    add spain
    add spain

---
+++ html/locales/es.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Tirar";
+invLocale.useItem = "Usar";
+invLocale.giveItem = "Dar";
+invLocale.secondInventoryNotAvailable = "El segundo inventario no esta disponible";
