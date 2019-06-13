56533422875ebceb07298c498d739b016d820940
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-06-13 11:46:19 +0300

    Create tr.lua
    added tr.lang

---
+++ locales/tr.lua
@@ -0,0 +1,11 @@
+Locales["tr"] = {
+	["cash"] = "Para",
+	["black_money"] = "Kara Para",
+	["player_nearby"] = "Bu oyuncu artık size yakın değil!",
+	["players_nearby"] = "Yakınlarda oyuncu yok!",
+	["openinv_help"] = "Başka bir oyuncunun envanterini açar",
+	["openinv_id"] = "Oyuncu ID",
+	["no_permissions"] = "Bunu yapmak için iznin yok!",
+	["no_player"] = "Oyuncu bulamadı! ID'yi doğru yazdığına emin ol.",
+	["player_inventory"] = "Oyuncu envanteri"
+}
