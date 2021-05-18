ab1b27a61f16d2f1b523a41ae6729dbe8ead87e5
fabiorodriguesroque <fabioroque9@gmail.com>
2021-05-18 19:29:20 +0100

    Add pt language to the inventoryhud

---
+++ html/locales/pt.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Dropar";
+invLocale.useItem = "Usar";
+invLocale.giveItem = "Dar";
+invLocale.secondInventoryNotAvailable = "Segundo Invetário não está disponível.";
